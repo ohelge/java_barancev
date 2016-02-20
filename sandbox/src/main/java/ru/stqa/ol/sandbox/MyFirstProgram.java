3c667241b08a8e01a982fd225b2fadcc6187d64d
OL <otestaa@gmail.com>
2016-02-20 23:54:52 +0100

    Functions added

--- sandbox/src/main/java/ru/stqa/ol/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ol/sandbox/MyFirstProgram.java
@@ -3,6 +3,23 @@ package ru.stqa.ol.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world");
+    hello("world");
+    hello("Oleg");
+    double a =5.0;
+        System.out.println("Square of " + a + " = " + area(a));
+    double b =6.0;
+    System.out.println("Square of " + a + " and " + b + " = " + area(a, b));
+  }
+
+  public static void hello(String smb) {
+    System.out.println("Hello, " + smb + "!");
+  }
+
+  public static double area (double len){
+        return len * len;
+  }
+
+  public static double area (double a, double b){
+    return a * b;
   }
 }
