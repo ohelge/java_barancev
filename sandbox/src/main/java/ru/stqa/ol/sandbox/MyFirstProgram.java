603531b9722c320e7cc1d17a357e84ee123ed488
OL <otestaa@gmail.com>
2016-02-21 00:50:06 +0100

    Moved functions to Classes

--- sandbox/src/main/java/ru/stqa/ol/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ol/sandbox/MyFirstProgram.java
@@ -7,20 +7,15 @@ public class MyFirstProgram {
     hello("Oleg");
     Square s = new Square (5);
 
-        System.out.println("Square of " + s.l + " = " + area(s));
+        System.out.println("Square of " + s.l + " = " + s.area());
     Rectangle r = new Rectangle(4.0, 6.0);
-     System.out.println("Square of " + r.a + " and " + r.b + " = " + area(r));
+     System.out.println("Square of " + r.a + " and " + r.b + " = " + r.area());
   }
 
   public static void hello(String smb) {
     System.out.println("Hello, " + smb + "!");
   }
 
-  public static double area (Square s){
-        return s.l * s.l;
-  }
 
-  public static double area (Rectangle r){
-    return r.a * r.b;
-  }
+
 }
