63332b302b1b08a61447df45bbe38117dd8d26fb
OL <otestaa@gmail.com>
2016-02-21 02:00:28 +0100

    Zadanie2 Functions not in Point class

--- sandbox/src/main/java/ru/stqa/ol/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ol/sandbox/MyFirstProgram.java
@@ -5,17 +5,24 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     hello("world");
     hello("Oleg");
-    Square s = new Square (5);
-
-        System.out.println("Square of " + s.l + " = " + s.area());
+    Square s = new Square(5);
+    System.out.println("Square of " + s.l + " = " + s.area());
     Rectangle r = new Rectangle(4.0, 6.0);
-     System.out.println("Square of " + r.a + " and " + r.b + " = " + r.area());
+    System.out.println("Square of " + r.a + " and " + r.b + " = " + r.area());
+
+    Point p1 = new Point(1, 1);
+    Point p2 = new Point(2, 2);
+
+    System.out.println("Distance between " + p1.x + " " + p1.y + " and " + p2.x + " " + p2.y + " = " + distance(p1, p2));
   }
 
   public static void hello(String smb) {
     System.out.println("Hello, " + smb + "!");
   }
 
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+  }
 
 
 }
---
+++ sandbox/src/main/java/ru/stqa/ol/sandbox/Point.java
@@ -0,0 +1,18 @@
+package ru.stqa.ol.sandbox;
+
+/**
+ * Created by OL on 21/02/2016.
+ */
+public class Point {
+
+  double x;
+  double y;
+
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+
+
+
+}
