32fadb32ed2d17749b2e86df2a2f00f219d50c3d
ohelge <otestaa@gmail.com>
2016-12-27 16:59:17 +0100

    l6_m4 final version
    4. Параметризация тестовых методов

--- addressbook-web-tests/src/test/java/ru/stqa/ol/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ol/addressbook/tests/GroupCreationTests.java
@@ -18,26 +18,24 @@ public class GroupCreationTests extends TestBase {
   @DataProvider
   public Iterator<Object[]> validGroups() {
     List<Object[]> list = new ArrayList<Object[]>(); //delaem spisok massivov objektov. Togda pri vipolnenii vidim 3 zapuska s 3mq testovimi dannimi.
-    list.add(new Object[]{"group1'", "header1", "footer1"}); //Esli postavim ' v imq to tol'ko odin test iz 3h upadet
-    list.add(new Object[]{"group2", "header2", "footer2"});
-    list.add(new Object[]{"group3", "header3", "footer3"});
+    list.add(new Object[]{new GroupData().withName("group1").withHeader("header1").withFooter("footer1")}); // Menqem v @DataProvider ne massiv strok a massiv objektov tipa GroupData, massiv budet iz odnogo elementa
+    list.add(new Object[]{new GroupData().withName("group2").withHeader("header2").withFooter("footer2")});
+    list.add(new Object[]{new GroupData().withName("group3").withHeader("header3").withFooter("footer3")});
     return list.iterator();
   }
 
-  @Test(enabled = true, dataProvider = "validGroups") //l6_m4 Pervoe:Parametr dataProvider ukazat' po imeni
-  public void testGroupCreation(String name, String header, String footer) { //l6_m4 Vtoroe: U testovogo metoda nado sdelat' stol'ko paramentrov skol'ko est' v kazhdom massive objektov.
-    /*String[] names = new String[]{"group1", "group2", "group3"}; //l6_m4 Cikl for mozhno ubrat'. Ego delaet TestNG
-    for (String name : names) { */
-      GroupData group = new GroupData().withName(name).withHeader(header).withFooter(footer);
-      app.goTo().groupPage();
-      Groups before = app.group().all(); //l5_m6
-      app.group().create(group);
+  @Test(enabled = true, dataProvider = "validGroups") //l6_m4 Parametr dataProvider ukazat' po imeni
+  public void testGroupCreation(GroupData group) { //l6_m4 Udobnee peredavat' dannie kak 1 objekt: menqem String name, String header, String footer na GroupData group.
+    // Menqem v @DataProvider ne massiv strok a massiv objektov tipa GroupData, massiv budet iz odnogo elementa
+    app.goTo().groupPage();
+    Groups before = app.group().all(); //l5_m6
+    app.group().create(group);
 
-      assertThat(app.group().count(), equalTo(before.size() + 1)); //l5_m8 : zamenqem after.size() na bolee bistruu proverku app.group().count() i stavim pered zagruzkoi spiska grupp. Eto i est' hashirovanie
-      Groups after = app.group().all();
+    assertThat(app.group().count(), equalTo(before.size() + 1)); //l5_m8 : zamenqem after.size() na bolee bistruu proverku app.group().count() i stavim pered zagruzkoi spiska grupp. Eto i est' hashirovanie
+    Groups after = app.group().all();
 
-      group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());   //l5_m5: prewvawaem potok objektov GrouData v potok Identifikatorov. Anonimnaq funkciq mapToInt g: parametr gruppa, a resultat - identifikator. Berem max i preobrazuem v int
-      assertThat(after, equalTo(before.withAdded(group))); // MatcherAssert.assertThat(..) Alt+Enter -> "Add static import"
+    group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());   //l5_m5: prewvawaem potok objektov GrouData v potok Identifikatorov. Anonimnaq funkciq mapToInt g: parametr gruppa, a resultat - identifikator. Berem max i preobrazuem v int
+    assertThat(after, equalTo(before.withAdded(group))); // MatcherAssert.assertThat(..) Alt+Enter -> "Add static import"
 
   }
 
